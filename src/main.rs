fdd118e884acef04bd6c17efa83eb594363c07ce
aQaTL <mmsoltys@outlook.com>
2023-10-19 22:33:32 +0200

    Init bare repo

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,54 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+use thiserror::Error;
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+fn main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	println!("Logs from your program will appear here!");
+
+	// Uncomment this block to pass the first stage
+	let args: Vec<String> = env::args().collect();
+	if args.len() <= 1 {
+		print_usage();
+		std::process::exit(1);
+	}
+
+	let result: Result<(), Box<dyn std::error::Error>> = match args[1].as_str() {
+		"init" => init().map_err(Into::into),
+		cmd => Err(anyhow::anyhow!("unknown command: {cmd}").into()),
+	};
+
+	if let Err(err) = result {
+		println!("{err}");
+		std::process::exit(1);
+	}
+}
+
+fn print_usage() {
+	println!(
+		"\
+Usage: git-starter-rust [COMMAND]
+
+COMMAND:
+	init
+\
+		"
+	);
+}
+
+#[derive(Debug, Error)]
+enum InitError {
+	#[error(transparent)]
+	Io(#[from] std::io::Error),
+}
+
+fn init() -> Result<(), InitError> {
+	fs::create_dir(".git")?;
+	fs::create_dir(".git/objects")?;
+	fs::create_dir(".git/refs")?;
+	fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
+	println!("Initialized git directory");
+
+	Ok(())
 }
