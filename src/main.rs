f013760d768fdd973817172399af1b0807fbcbf6
aQaTL <mmsoltys@outlook.com>
2023-10-20 12:51:52 +0200

    Print contents of an blob object

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,49 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::{BufReader, Read};
 
+use clap::{Parser, Subcommand};
 use thiserror::Error;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+	#[command(subcommand)]
+	command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+	Init,
+	#[command(name = "cat-file")]
+	CatFile {
+		#[arg(short, long)]
+		pretty_print: bool,
+
+		#[arg(last = true)]
+		object: String,
+	},
+}
 
 fn main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	println!("Logs from your program will appear here!");
+	eprintln!("Logs from your program will appear here!");
 
-	// Uncomment this block to pass the first stage
-	let args: Vec<String> = env::args().collect();
-	if args.len() <= 1 {
-		print_usage();
-		std::process::exit(1);
-	}
+	let args = Args::parse();
 
-	let result: Result<(), Box<dyn std::error::Error>> = match args[1].as_str() {
-		"init" => init().map_err(Into::into),
-		cmd => Err(anyhow::anyhow!("unknown command: {cmd}").into()),
+	let result: Result<(), Box<dyn std::error::Error>> = match args.command {
+		Command::Init => init().map_err(Into::into),
+		Command::CatFile {
+			pretty_print,
+			object,
+		} => cat_file(pretty_print, object).map_err(Into::into),
 	};
 
 	if let Err(err) = result {
 		println!("{err}");
 		std::process::exit(1);
 	}
-}
-
-fn print_usage() {
-	println!(
-		"\
-Usage: git-starter-rust [COMMAND]
-
-COMMAND:
-	init
-\
-		"
-	);
 }
 
 #[derive(Debug, Error)]
@@ -50,7 +58,38 @@ fn init() -> Result<(), InitError> {
 	fs::create_dir(".git/objects")?;
 	fs::create_dir(".git/refs")?;
 	fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
-	println!("Initialized git directory");
+	eprintln!("Initialized git directory");
+
+	Ok(())
+}
+
+#[derive(Debug, Error)]
+enum CatFileError {
+	#[error("Not a valid object name {0}")]
+	InvalidObjectName(String),
+
+	#[error(transparent)]
+	Io(#[from] std::io::Error),
+}
+
+fn cat_file(pretty_print: bool, object: String) -> Result<(), CatFileError> {
+	eprintln!("pretty_print: {pretty_print}");
+	if object.len() != 40 {
+		return Err(CatFileError::InvalidObjectName(object));
+	}
+
+	let dirname = &object[0..2];
+	eprintln!("dirname: {dirname}");
+
+	let filename = &object[2..];
+	let file = fs::File::open(format!(".git/objects/{dirname}/{filename}"))?;
+	let file_buffered = BufReader::new(file);
+
+	let mut decoder = flate2::bufread::ZlibDecoder::new(file_buffered);
+	let mut file_contents = String::new();
+	decoder.read_to_string(&mut file_contents)?;
+
+	print!("{file_contents}");
 
 	Ok(())
 }
