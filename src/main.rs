9d7036c9587b43b754fb299b7842d4b83211e14e
aQaTL <mmsoltys@outlook.com>
2023-10-20 19:47:34 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -10,89 +10,89 @@ use thiserror::Error;
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[command(subcommand)]
-    command: Command,
+	#[command(subcommand)]
+	command: Command,
 }
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    Init,
-    #[command(name = "cat-file")]
-    CatFile {
-        #[arg(short, long)]
-        pretty_print: bool,
+	Init,
+	#[command(name = "cat-file")]
+	CatFile {
+		#[arg(short, long)]
+		pretty_print: bool,
 
-        #[arg(required = true)]
-        object: String,
-    },
+		#[arg(required = true)]
+		object: String,
+	},
 }
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	eprintln!("Logs from your program will appear here!");
 
-    let args = Args::parse();
+	let args = Args::parse();
 
-    let result: Result<(), Box<dyn std::error::Error>> = match args.command {
-        Command::Init => init().map_err(Into::into),
-        Command::CatFile {
-            pretty_print,
-            object,
-        } => cat_file(pretty_print, object).map_err(Into::into),
-    };
+	let result: Result<(), Box<dyn std::error::Error>> = match args.command {
+		Command::Init => init().map_err(Into::into),
+		Command::CatFile {
+			pretty_print,
+			object,
+		} => cat_file(pretty_print, object).map_err(Into::into),
+	};
 
-    if let Err(err) = result {
-        println!("{err}");
-        std::process::exit(1);
-    }
+	if let Err(err) = result {
+		println!("{err}");
+		std::process::exit(1);
+	}
 }
 
 #[derive(Debug, Error)]
 enum InitError {
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
+	#[error(transparent)]
+	Io(#[from] std::io::Error),
 }
 
 fn init() -> Result<(), InitError> {
-    fs::create_dir(".git")?;
-    fs::create_dir(".git/objects")?;
-    fs::create_dir(".git/refs")?;
-    fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
-    eprintln!("Initialized git directory");
+	fs::create_dir(".git")?;
+	fs::create_dir(".git/objects")?;
+	fs::create_dir(".git/refs")?;
+	fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
+	eprintln!("Initialized git directory");
 
-    Ok(())
+	Ok(())
 }
 
 #[derive(Debug, Error)]
 enum CatFileError {
-    #[error("Not a valid object name {0}")]
-    InvalidObjectName(String),
+	#[error("Not a valid object name {0}")]
+	InvalidObjectName(String),
 
-    #[error(transparent)]
-    Io(#[from] std::io::Error),
+	#[error(transparent)]
+	Io(#[from] std::io::Error),
 }
 
 fn cat_file(pretty_print: bool, object: String) -> Result<(), CatFileError> {
-    eprintln!("pretty_print: {pretty_print}");
-    if object.len() != 40 {
-        return Err(CatFileError::InvalidObjectName(object));
-    }
+	eprintln!("pretty_print: {pretty_print}");
+	if object.len() != 40 {
+		return Err(CatFileError::InvalidObjectName(object));
+	}
 
-    let dirname = &object[0..2];
-    eprintln!("dirname: {dirname}");
+	let dirname = &object[0..2];
+	eprintln!("dirname: {dirname}");
 
-    let filename = &object[2..];
-    let file = fs::File::open(format!(".git/objects/{dirname}/{filename}"))?;
-    let file_buffered = BufReader::new(file);
+	let filename = &object[2..];
+	let file = fs::File::open(format!(".git/objects/{dirname}/{filename}"))?;
+	let file_buffered = BufReader::new(file);
 
-    let mut decoder = flate2::bufread::ZlibDecoder::new(file_buffered);
-    let mut file_content_bytes = Vec::new();
+	let mut decoder = flate2::bufread::ZlibDecoder::new(file_buffered);
+	let mut file_content_bytes = Vec::new();
 	decoder.read_to_end(&mut file_content_bytes)?;
 	let null_pos = file_content_bytes.iter().position(|x| *x == 0).unwrap();
 
 	let file_content = std::str::from_utf8(&file_content_bytes[(null_pos + 1)..]).unwrap();
 
-    print!("{file_content}");
+	print!("{file_content}");
 
-    Ok(())
+	Ok(())
 }
